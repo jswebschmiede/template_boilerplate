5b1b424241c6246c35706122bb23ec125ab938ef
jswebschiede <schoeneburg.j@googlemail.com>
2024-10-21 11:24:39 +0200

    cleanup

--- src/media/js/vendor/preloader.js
+++ src/media/js/vendor/preloader.js
@@ -1,18 +1,12 @@
 // File#: _1_preloader
 
-(function () {
-  setTimeout(() => {
-    const preloader = document.getElementById("loader");
-    preloader?.classList.add("done");
+(() => {
+    setTimeout(() => {
+        const preloader = document.getElementById('loader');
+        preloader?.classList.add('done');
 
-    new Promise((resolve) => setTimeout(resolve, 500)).then(() => {
-      preloader?.remove();
-      document
-        .querySelector("[data-animate-headline]")
-        ?.classList.add("animate__animated", "animate__fadeInDown");
-      document
-        .querySelector("[data-animate-scroll]")
-        ?.classList.add("animate__animated", "animate__fadeInUp");
-    });
-  }, 500);
+        new Promise((resolve) => setTimeout(resolve, 500)).then(() => {
+            preloader?.remove();
+        });
+    }, 500);
 })();
